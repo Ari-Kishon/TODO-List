45fe4d7db7a31275e0ee595718ec8c75a7260d0f
Ari Kishon <ariki@wix.com>
2020-05-07 12:39:53 +0300

    improved implementation of get request handling

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
-const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const tasks = [];
+const getTaskByID = (id) => tasks.find((obj) => obj.id == id);
 
 const server = express();
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
 server.get("/", (req, res) => res.send("no path specified"));
 server.get("/tasks", (req, res) => res.send(JSON.stringify(tasks)));
-server.get("/tasks/:id", function (req, res) {
-  const id = req.params.id;
+server.get("/tasks/:id", getTaskFromServer);
+server.listen(8080);
+
+function getTaskFromServer({
+  params: {
+    id
+  }
+}, res) {
   const task = tasks.find((obj) => obj.id == id);
   if (task) {
     res.send(task.task);
   } else {
+    res.status(500);
     res.send(`task:"${id}" was not found`);
   }
-});
-server.listen(8080);
+}
