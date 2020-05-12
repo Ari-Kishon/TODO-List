a0cc3f13b6e24423ea53f0eace013d30eaabd670
Ari Kishon <ariki@wix.com>
2020-05-12 14:36:31 +0300

    Refined error handling

--- client/task-manager.js
+++ client/task-manager.js
@@ -1,5 +1,7 @@
-const trashSVG = "<svg height=\"30pt\" viewBox=\"-48 0 407 407\" width=\"30pt\" xmlns=\"http: //www.w3.org/2000/svg\"><path d=\"m89.199219 37c0-12.132812 9.46875-21 21.601562-21h88.800781c12.128907 0 21.597657 8.867188 21.597657 21v23h16v-23c0-20.953125-16.644531-37-37.597657-37h-88.800781c-20.953125 0-37.601562 16.046875-37.601562 37v23h16zm0 0\"/><path d=\"m60.601562 407h189.199219c18.242188 0 32.398438-16.046875 32.398438-36v-247h-254v247c0 19.953125 14.15625 36 32.402343 36zm145.597657-244.800781c0-4.417969 3.582031-8 8-8s8 3.582031 8 8v189c0 4.417969-3.582031 8-8 8s-8-3.582031-8-8zm-59 0c0-4.417969 3.582031-8 8-8s8 3.582031 8 8v189c0 4.417969-3.582031 8-8 8s-8-3.582031-8-8zm-59 0c0-4.417969 3.582031-8 8-8s8 3.582031 8 8v189c0 4.417969-3.582031 8-8 8s-8-3.582031-8-8zm0 0\"/><path d=\"m20 108h270.398438c11.046874 0 20-8.953125 20-20s-8.953126-20-20-20h-270.398438c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20zm0 0\"/></svg>"
-const checkSVG = "<svg height=\"30pt\" viewBox=\"0 -46 417.81333 417\" width=\"30pt\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"m159.988281 318.582031c-3.988281 4.011719-9.429687 6.25-15.082031 6.25s-11.09375-2.238281-15.082031-6.25l-120.449219-120.46875c-12.5-12.5-12.5-32.769531 0-45.246093l15.082031-15.085938c12.503907-12.5 32.75-12.5 45.25 0l75.199219 75.203125 203.199219-203.203125c12.503906-12.5 32.769531-12.5 45.25 0l15.082031 15.085938c12.5 12.5 12.5 32.765624 0 45.246093zm0 0\"/></svg>"
+const trashSVG =
+  '<svg height="30pt" viewBox="-48 0 407 407" width="30pt" xmlns="http: //www.w3.org/2000/svg"><path d="m89.199219 37c0-12.132812 9.46875-21 21.601562-21h88.800781c12.128907 0 21.597657 8.867188 21.597657 21v23h16v-23c0-20.953125-16.644531-37-37.597657-37h-88.800781c-20.953125 0-37.601562 16.046875-37.601562 37v23h16zm0 0"/><path d="m60.601562 407h189.199219c18.242188 0 32.398438-16.046875 32.398438-36v-247h-254v247c0 19.953125 14.15625 36 32.402343 36zm145.597657-244.800781c0-4.417969 3.582031-8 8-8s8 3.582031 8 8v189c0 4.417969-3.582031 8-8 8s-8-3.582031-8-8zm-59 0c0-4.417969 3.582031-8 8-8s8 3.582031 8 8v189c0 4.417969-3.582031 8-8 8s-8-3.582031-8-8zm-59 0c0-4.417969 3.582031-8 8-8s8 3.582031 8 8v189c0 4.417969-3.582031 8-8 8s-8-3.582031-8-8zm0 0"/><path d="m20 108h270.398438c11.046874 0 20-8.953125 20-20s-8.953126-20-20-20h-270.398438c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20zm0 0"/></svg>';
+const checkSVG =
+  '<svg height="30pt" viewBox="0 -46 417.81333 417" width="30pt" xmlns="http://www.w3.org/2000/svg"><path d="m159.988281 318.582031c-3.988281 4.011719-9.429687 6.25-15.082031 6.25s-11.09375-2.238281-15.082031-6.25l-120.449219-120.46875c-12.5-12.5-12.5-32.769531 0-45.246093l15.082031-15.085938c12.503907-12.5 32.75-12.5 45.25 0l75.199219 75.203125 203.199219-203.203125c12.503906-12.5 32.769531-12.5 45.25 0l15.082031 15.085938c12.5 12.5 12.5 32.765624 0 45.246093zm0 0"/></svg>';
 
 const taskList = document.querySelector(".taskList");
 const submitButton = document.querySelector(".submitButton");
@@ -7,127 +9,96 @@ const newTaskInput = document.querySelector(".newTaskInput");
 
 const tasks = [];
 
-//
-////
-////// PROMISES
-////
-//
-const promiseGetTasks = () => {
-    return fetch(`/tasks`, {
-            headers: {
-                "Accept": "application/json"
-            },
-            method: 'GET'
-        })
-        .then((response) => {
-            return response.json()
-        })
-        .catch(error => {
-            throw (error);
-        })
-}
-const promiseCreateTask = (text) => {
-    return fetch('/tasks', {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                "Text": text
-            })
-        })
-        .then((response) => {
-            return response.json()
-        })
-        .catch(error => {
-            throw (error);
-        })
-}
-const promiseToggleTask = (id) => {
-    return fetch('/tasks/complete', {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                "id": id
-            })
-        })
-        .then((response) => {
-            return response.json()
-        })
-        .catch(error => {
-            throw (error);
-        })
-}
-const promiseRenameTask = (id, text) => {
-    return fetch('/tasks/rename', {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                "id": id,
-                "Text": text
-            })
-        })
-        .catch(error => {
-            throw (error);
-        })
-}
-const promiseRemoveTask = (id) => {
-    return fetch('/tasks/remove', {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                "id": id
-            })
-        })
-        .catch(error => {
-            throw (error);
-        })
-}
+const displayError = (message) => {
+  document.body.appendChild(createErrorElement(message));
+};
 
-//
-////
-////// ASYNC FUNCTIONS
-////
-//
+const getTaskElement = (id) => {
+  const task = taskList.querySelector(`.task[uid="${id}"]`);
+  if (task) {
+    return task;
+  } else {
+    throw new Error(`Could not find task #${id}`);
+  }
+};
+
 async function loadAllTasks() {
-    const list = await promiseGetTasks();
-    for (task of list) {
-        taskList.appendChild(createTaskElement(task.id, task.text, task.complete));
-    }
+  const list = await (
+    await fetch(`/tasks`, {
+      headers: {
+        Accept: "application/json",
+      },
+      method: "GET",
+    })
+  ).json();
+  for (task of list) {
+    taskList.appendChild(createTaskElement(task.id, task.text, task.complete));
+  }
 }
 
 async function createTask(text) {
-    if (!taskList || !newTaskInput) {
-        throw new Error(`newInput ot TaskList could not be found`);
-    }
-    const taskID = await promiseCreateTask(text);
-    taskList.appendChild(createTaskElement(taskID, text));
-    newTaskInput.value = "";
+  if (!taskList || !newTaskInput) {
+    throw new Error(`newInput ot TaskList could not be found`);
+  }
+  let taskID = await (
+    await fetch("/tasks", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        Text: text,
+      }),
+    })
+  ).json();
+  taskList.appendChild(createTaskElement(taskID, text));
+  newTaskInput.value = "";
 }
 
 async function toggleTask(id) {
-    isComplete = await promiseToggleTask(id);
-    const taskElement = document.querySelector(`.task[uid="${id}"]`)
-    const inputElement = document.querySelector(`.task[uid="${id}"] input`);
-    if (!taskElement || !inputElement) {
-        throw new Error('task element could not be found');
-    }
-    taskElement.setAttribute('complete', isComplete);
-    inputElement.disabled = !isComplete;
+  const inputElement = document.querySelector(`.task[uid="${id}"] input`);
+  const taskElement = document.querySelector(`.task[uid="${id}"]`);
+  if (!taskElement || !inputElement) {
+    throw new Error("task element could not be found");
+  }
+  const isComplete = await (
+    await fetch("/tasks/complete", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        id: id,
+      }),
+    })
+  ).json();
+  taskElement.setAttribute("complete", isComplete);
+  inputElement.disabled = !isComplete;
 }
 
 async function renameTask(id, text) {
-    await promiseRenameTask(id, text);
+  await fetch("/tasks/rename", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      id: id,
+      Text: text,
+    }),
+  });
 }
 async function removeTask(id) {
-    await promiseRemoveTask(id);
-    taskList.querySelector(`.task[uid="${id}"]`).remove();
+  await fetch("/tasks/remove", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      id: id,
+    }),
+  });
+  getTaskElement(id).remove();
 }
 
 //
@@ -136,60 +107,80 @@ async function removeTask(id) {
 ////
 //
 const createTaskElement = (id, text, complete = false) => {
-    const task = document.createElement("li");
-    task.className = "task"
-    task.setAttribute("complete", complete);
-    task.setAttribute("uid", id);
-    task.appendChild(createInput(id, text));
-    task.appendChild(createFinishButton(id));
-    task.appendChild(createTrashButton(id));
-    return task;
-}
+  const task = document.createElement("li");
+  task.className = "task";
+  task.setAttribute("complete", complete);
+  task.setAttribute("uid", id);
+  task.appendChild(createInput(id, text));
+  task.appendChild(createFinishButton(id));
+  task.appendChild(createTrashButton(id));
+  return task;
+};
 const createInput = (id, text) => {
-    const input = document.createElement("input");
-    input.className = ("taskTitle");
-    input.value = text;
-    input.placeholder = "EMPTY";
-    input.addEventListener('input', (e) => {
-        renameTask(id, e.target.value)
-    })
-    return input;
-}
+  const input = document.createElement("input");
+  input.className = "taskTitle";
+  input.value = text;
+  input.placeholder = "EMPTY";
+  input.addEventListener("input", (e) => {
+    renameTask(id, e.target.value).catch(() => {
+      displayError("Communication Error: could not rename task in server");
+    });
+  });
+  return input;
+};
 const createFinishButton = (id) => {
-    const button = document.createElement("button");
-    button.className = "finishButton";
-    button.innerHTML = checkSVG;
-    button.addEventListener('click', () => {
-        toggleTask(id);
+  const button = document.createElement("button");
+  button.className = "finishButton";
+  button.innerHTML = checkSVG;
+  button.addEventListener("click", () => {
+    toggleTask(id).catch(() => {
+      displayError(
+        "Communication Error: could not toggle task status in server"
+      );
     });
-    return button;
-}
+  });
+  return button;
+};
 const createTrashButton = (id) => {
-    const button = document.createElement("button");
-    button.className = "trashButton";
-    button.innerHTML = trashSVG;
-    button.addEventListener('click', () => {
-        removeTask(id);
+  const button = document.createElement("button");
+  button.className = "trashButton";
+  button.innerHTML = trashSVG;
+  button.addEventListener("click", () => {
+    removeTask(id).catch(() => {
+      displayError("Communication Error: could not remove task from server");
     });
-    return button;
-}
-
+  });
+  return button;
+};
+const createErrorElement = (message) => {
+  const error = document.createElement("Div");
+  error.className = "errorPopUp";
+  error.innerHTML = message;
+  error.addEventListener("click", () => {
+    error.remove();
+  });
+  setTimeout(() => error.remove(), 5000);
+  return error;
+};
 //
 ////
 ////// EVENT LISTENERS
 ////
 //
 if (!submitButton) {
-    throw new Error(`could not find the submit button`);
+  throw new Error(`could not find the submit button`);
 }
-submitButton.addEventListener('click', () => {
-    if (newTaskInput.value) {
-        createTask(newTaskInput.value);
-    } else {
-        newTaskInput.placeholder = "please write a task";
-    }
+submitButton.addEventListener("click", () => {
+  if (newTaskInput.value) {
+    createTask(newTaskInput.value).catch(() => {
+      displayError("Communication Error: could not create task in server");
+    });
+  } else {
+    newTaskInput.placeholder = "please write a task";
+  }
 });
 
-//WHEN SHOULD I REFRESH TASKS???
-//LOAD TASKS ON SERVER LOAD
-loadAllTasks();
+//LOAD TASKS ON INIT
+loadAllTasks().catch(() => {
+  displayError("Communication Error: could not load from server");
+});
--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const D_PORT = 8080;
+
 const tasks = [];
-const addNewTask = (id, text) => tasks.push({
-  id,
-  text,
-  complete: false,
-});
+const addNewTask = (id, text) =>
+  tasks.push({
+    id,
+    text,
+    complete: false,
+  });
 
 //
 ////
@@ -16,81 +19,92 @@ const addNewTask = (id, text) => tasks.push({
 //
 // server setup
 const server = express();
-server.use(express.static('..'));
+server.use(express.static(path.join(__dirname, "..")));
 server.use(bodyParser.json());
 // GET requests
-server.get("/", (req, res) => res.sendFile(path.join(__dirname, '../app', 'index.html')));
-server.get("/tasks", getAllTasksFromServer);
-server.get("/tasks/:id", getTaskFromServer);
+server.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../app", "index.html"))
+);
+server.get("/tasks", getAllTasks);
+server.get("/tasks/:id", getTask);
 // POST requests
 server.post("/", (req, res) => res.send("no path specified"));
-server.post("/tasks", postTaskToServer);
-server.post("/tasks/complete", toggleTaskInServer);
-server.post("/tasks/rename", renameTaskInServer);
-server.post("/tasks/remove", removeTaskFromServer);
+server.post("/tasks", saveTask);
+server.post("/tasks/complete", toggleTask);
+server.post("/tasks/rename", renameTask);
+server.post("/tasks/remove", removeTask);
 // server action
-server.listen(8080);
-
+server.listen(D_PORT, () =>
+  console.log(`The server is listening on port :${D_PORT}`)
+);
 
 //
 ///
 ///// SERVER FUNCTIONS
 ///
 //
-function getTaskFromServer({
-  params: {
-    id
-  }
-}, res) {
+function getTask({ params: { id } }, res) {
   const task = tasks.find((obj) => obj.id === id);
   if (task) {
-    res.send(task);
+    res.status(200).send(task);
   } else {
     res.status(500).send(`task:"${id}" was not found`);
   }
-}
-
-function getAllTasksFromServer(req, res) {
-  res.send((JSON.stringify(tasks)));
   res.end();
 }
 
-function postTaskToServer(req, res) {
+function getAllTasks(req, res) {
+  res.send(JSON.stringify(tasks));
+  res.status(200).end();
+}
+
+function saveTask(req, res) {
   const text = req.body.Text;
   if (text) {
     const id = Date.now();
-    addNewTask(id, text)
-    res.send(`${id}`);
+    addNewTask(id, text);
+    res.status(200).send(`${id}`);
   } else {
-    res.status(500);
-    res.send("invalid request");
+    res.status(500).send("invalid request");
   }
   res.end();
 }
 
-function toggleTaskInServer(req, res) {
+function toggleTask(req, res) {
   const id = req.body.id;
   const task = tasks.find((obj) => obj.id === id);
-  task.complete = !task.complete
-  res.end(`${task.complete}`);
-}
-
-function renameTaskInServer(req, res) {
-  const task = tasks.find((obj) => obj.id === req.body.id);
   if (task) {
-    task.text = req.body.Text;
+    task.complete = !task.complete;
+    res.status(200).send(`${task.complete}`);
   } else {
     res.status(500).send(`task:"${id}" was not found`);
   }
   res.end();
 }
 
-function removeTaskFromServer(req, res) {
-  const id = req.body.id;
-  const index = tasks.findIndex((obj) => obj.id === id);
-  if (index < 0) {
-    throw new Error('could not remove task')
+function renameTask(req, res) {
+  const task = tasks.find((obj) => obj.id === req.body.id);
+  if (task) {
+    task.text = req.body.Text;
+    res.status(200);
+  } else {
+    res.status(500).send(`task:"${id}" was not found`);
   }
-  tasks.splice(index, 1);
+  res.end();
+}
+
+function removeTask(req, res) {
+  const id = req.body.id;
+  if (id) {
+    const index = tasks.findIndex((obj) => obj.id === id);
+    if (index < 0) {
+      res.status(500).send(`task:"${id}" was not found`);
+    }
+    tasks.splice(index, 1);
+    res.status(200);
+  } else {
+    res.status(500).send("invalid request");
+  }
+
   res.end();
 }
